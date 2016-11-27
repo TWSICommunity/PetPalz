e33681f6252e5ab92df68ce254ee7e4fb9320935
TWSICommunity <gamewolf10@gmail.com>
2016-11-27 00:04:10 -0800

    Redoing this later...
    Welp I suppose using functions to send messages in petpalz site will have to come by later. I'll have to start using functions which will help in the long run.

--- chat.php
+++ chat.php
@@ -1,23 +1,3 @@
 <?php
-//echo "Chat is in work of progress.";
-$dbhost = "localhost";
-$dbuser = "user";
-$dbpass = "pass";
-$dbname = "yourdatabasenamehere";
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-$username = "";
-
-if(!$conn) {
- echo "Failed to connect!";
- exit;//If the php error pops up then add a () at the end of exit like this "exit();"
-}
-
-mysql_select_db($dbname);
-
-$username = $_COOKIE['username'];//This cookie should be set at login.php otherwise there is no way for the user to chat.
-if ($username == "") {
- echo "Please login before chatting.";
- exit;//Same as before with the exit if all fails.
-} else {
- if(isset($_POST
+ ///Functions needed to be added. like for instance SendChatMsg($Username,$YourInput);
 ?>
